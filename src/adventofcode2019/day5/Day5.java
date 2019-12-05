ae9821192d77561967db7edd45630081185098e8
carlos Lopez Rodriguez <carloslopezrodriguez94@gmail.com>
2019-12-05 21:26:55 +0100

    Added day 5 part 2

--- src/adventofcode2019/day5/Day5.java
+++ src/adventofcode2019/day5/Day5.java
@@ -17,25 +17,36 @@ public class Day5 {
 
     public void main() {
         System.out.println("Day 5 - Part 1");
+        //Read input
         String input = Util.ReadFileOneLine("day5/input.txt");
         List<Integer> instructions = inputToIntegerList(input);
-        evaluate(instructions, 1);
+
+        System.out.println("Evaluation with a input of '1': ");
+        List<Integer> output = evaluate(instructions, 1);
+        for (Integer out : output) {
+            if (out != 0) {
+                System.out.println(out);
+            }
+        }
+
+        System.out.println("Day 5 - Part 2");
+        // Reset input
+        instructions = inputToIntegerList(input);
+        
+        System.out.println("Evaluation with a input of '5': ");
+        output = evaluate(instructions, 5);
+        System.out.println(output.get(0));
     }
 
-    private void evaluate(List<Integer> instructions, int input) {
-        //Replace position 1 with value '12' and position 2 with value '2'
-//        list.set(1, noun);
-        //      list.set(2, verb);
+    private List<Integer> evaluate(List<Integer> instructions, int input) {
         List<Integer> output = new ArrayList<>();
         int pointer = 0;
         boolean finish = false;
         while (!finish) {
             int instruction = instructions.get(pointer);
-            System.out.println(instruction);
             int opcode = instruction % 100;
             boolean firstParamImmediate = ((instruction / 100) % 10) == 1;
             boolean secondParamImmediate = ((instruction / 1000) % 10) == 1;
-            boolean thirdParamImmediate = false;// ((instruction / 10000) % 10) == 1;
 
             int param1;
             int param2;
@@ -50,13 +61,10 @@ public class Day5 {
 
                     param3 = getNextInput(instructions, pointer + 3);
 
-                    System.out.println(instruction + " --> (" + param1 + ", " + param2 + ", " + param3 + ") --> guarda " + (param1 + param2) + " en position " + param3);
-
                     instructions.set(param3, param1 + param2);
 
                     //increse pointer
                     pointer += 4;
-
                     break;
                 case 2:
                     param1 = getNextInput(instructions, pointer + 1);
@@ -67,8 +75,6 @@ public class Day5 {
 
                     param3 = getNextInput(instructions, pointer + 3);
 
-                    System.out.println(instruction + " --> (" + param1 + ", " + param2 + ", " + param3 + ")  --> guarda " + (param1 * param2) + " en position " + param3);
-
                     instructions.set(param3, param1 * param2);
 
                     //increse pointer
@@ -78,8 +84,6 @@ public class Day5 {
                     param1 = getNextInput(instructions, pointer + 1);
                     instructions.set(param1, input);
 
-                    System.out.println(instruction + " --> guarda '" + input + "' en (" + param1 + ")");
-
                     //increse pointer
                     pointer += 2;
                     break;
@@ -88,10 +92,66 @@ public class Day5 {
                     param1 = getValue(instructions, param1, firstParamImmediate);
                     output.add(param1);
 
-                    System.out.println(instruction + " --> imprime (" + param1 + ")");
-
                     //increse pointer
                     pointer += 2;
+                    break;
+                case 5:
+                    param1 = getNextInput(instructions, pointer + 1);
+                    param1 = getValue(instructions, param1, firstParamImmediate);
+
+                    if (param1 != 0) {
+                        param2 = getNextInput(instructions, pointer + 2);
+                        param2 = getValue(instructions, param2, secondParamImmediate);
+
+                        pointer = param2;
+                    } else {
+                        pointer += 3;
+                    }
+                    break;
+                case 6:
+                    param1 = getNextInput(instructions, pointer + 1);
+                    param1 = getValue(instructions, param1, firstParamImmediate);
+
+                    if (param1 == 0) {
+                        param2 = getNextInput(instructions, pointer + 2);
+                        param2 = getValue(instructions, param2, secondParamImmediate);
+
+                        pointer = param2;
+                    } else {
+                        pointer += 3;
+                    }
+                    break;
+                case 7:
+                    param1 = getNextInput(instructions, pointer + 1);
+                    param1 = getValue(instructions, param1, firstParamImmediate);
+
+                    param2 = getNextInput(instructions, pointer + 2);
+                    param2 = getValue(instructions, param2, secondParamImmediate);
+
+                    param3 = getNextInput(instructions, pointer + 3);
+
+                    if (param1 < param2) {
+                        instructions.set(param3, 1);
+                    } else {
+                        instructions.set(param3, 0);
+                    }
+                    pointer += 4;
+                    break;
+                case 8:
+                    param1 = getNextInput(instructions, pointer + 1);
+                    param1 = getValue(instructions, param1, firstParamImmediate);
+
+                    param2 = getNextInput(instructions, pointer + 2);
+                    param2 = getValue(instructions, param2, secondParamImmediate);
+
+                    param3 = getNextInput(instructions, pointer + 3);
+
+                    if (param1 == param2) {
+                        instructions.set(param3, 1);
+                    } else {
+                        instructions.set(param3, 0);
+                    }
+                    pointer += 4;
                     break;
                 case 99:
                     finish = true;
@@ -99,10 +159,7 @@ public class Day5 {
             }
         }
 
-        for (Integer code : output) {
-            System.out.print(code);
-        }
-        System.out.println("\n" + output);
+        return output;
     }
 
     private int getNextInput(List<Integer> instructions, int pointer) {
@@ -128,6 +185,6 @@ public class Day5 {
         }
 
         return list;
-    }  
-    
+    }
+
 }
